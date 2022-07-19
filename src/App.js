85de62e76832e6f51882be209af0f406370860b0
ocki982 <maiderocta@gmail.com>
2022-07-18 20:32:17 -0700

    started to create homepage

--- src/App.js
+++ src/App.js
@@ -61,7 +61,6 @@ const App = () => {
             <div className="fixed md:static bg-main-bg dark:bg-main-dark-bg navbar w-full">
               <Navbar />
             </div>
-          </div>
 
           <div>
             <Routes>
@@ -91,6 +90,8 @@ const App = () => {
               <Route path="/stacked" element={<Stacked />} />
             </Routes>
           </div>
+          </div>
+
         </div>
       </BrowserRouter>
     </div>
