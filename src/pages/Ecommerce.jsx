3d5ba28c0faf93c363330681d2ab03472b90ad75
ocki982 <maiderocta@gmail.com>
2022-07-19 18:43:45 -0700

    building revenue graph

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -52,15 +52,32 @@ const Ecommerce = () => {
               <p className="text-sm text-gray-400 mt-1">
                 {item.title}
               </p>
-
-
             </div>
           ))}
         </div>
       </div>
 
-      <div>
-        
+      <div className="flex gap-10 flex-wrap justify-center">
+        <div className="bg-white dark:text-gray-200 dark:bg-secondary-dark-bg m-3 p-4 rounded-2xl md:w-780">
+            <div className="flex justify-between">
+              <p className="font-semibold text-xl">
+                Revenue Updates
+              </p>
+              <div className="flex items-center gap-4">
+                <p className="flex items-center gap-2 text-gray-600 hover:drop-shadow-xl">
+                  <span><GoPrimitiveDot />
+                  </span>
+                  <span>Expense</span>
+                </p>
+                <p className="flex items-center gap-2 text-green-400 hover:drop-shadow-xl">
+                  <span><GoPrimitiveDot />
+                  </span>
+                  <span>Budget</span>
+                </p>
+              </div>
+
+            </div>
+        </div>
       </div>
 
     </div>
